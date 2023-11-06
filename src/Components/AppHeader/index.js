21361be169e1cb03bbd01c19ca2d35fe59cebc5a
Eswar030304 <ivaturieswar334@gmail.com>
2023-11-06 07:57:52 +0530

    first commit

--- src/Components/AppHeader/index.js
+++ src/Components/AppHeader/index.js
@@ -22,7 +22,7 @@ function AppHeader() {
     <div className="AppHeader">
       <Image
         width={40}
-        src="https://yt3.ggpht.com/ytc/AMLnZu83ghQ28n1SqADR-RbI2BGYTrqqThAtJbfv9jcq=s176-c-k-c0x00ffffff-no-rj"
+        src= ""
       ></Image>
       <Typography.Title>Inventory Managment System</Typography.Title>
       <Space>
